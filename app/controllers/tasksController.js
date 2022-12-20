fba4e9a1d8360d618e951769767337ab7fd8c788
RemOclock <remi.sulpice@oclock.io>
2022-12-20 10:51:13 +0000

    Architecture : Fonctions métiers déplacées dans le controllers/tasksController.js

---
+++ app/controllers/tasksController.js
@@ -0,0 +1,44 @@
+const tasks = require('../data/tasks.json');
+
+const tasksController = {
+  getAllTasks: (req, res) => {
+    console.log(`${req.method} ${req.originalUrl}`);
+    res.send(tasks);
+  },
+  getTaskById: (req, res) => {
+    console.log(`${req.method} ${req.originalUrl}`);
+    const taskId = parseInt(req.params.id, 10);
+    const task = tasks.find((task) => task.id === taskId);
+    res.json(task);
+  },
+  createTask: (req, res) => {
+    console.log(`${req.method} ${req.originalUrl}`);
+
+    if(req.body.label === undefined || req.body.label === "") {
+      return res.status(400).end('The task\'s name is empty');
+    };
+
+    // On prépare la nouvelle tâche
+    const newTask = {
+      id: 99, // On vera plus tard comment faire ça prorpement
+      label: req.body.label,
+      done: false,
+      userId: 1
+    };
+
+    // On l'a rajoute dans le tableau des tâches
+    try {
+      tasks.push(newTask);
+      // Si la création de la ressource est ok
+      // on répond un code 201 Created et on envoi la nouvelle tâche
+      res.status(201).json(newTask);
+    } catch (error) {
+      // Si la création ne passe pas
+      // on répond un code 500 Internal Error et on termine le processus de réponse
+      console.error(error);
+      res.status(500).end(error);
+    }
+  },
+};
+
+module.exports = tasksController;
