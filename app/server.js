069753d69e76c299a810c84c44ab171a008a44ce
RemOclock <remi.sulpice@oclock.io>
2022-12-20 10:36:10 +0000

    Architecture : Router contenant nos routes/endpoints. On utilise l'objet Router pour gérer nos routes dans le fichier app/router.js. On branche notre router dans la chaine de middlewares

--- app/server.js
+++ app/server.js
@@ -1,6 +1,8 @@
 // Packages
 const express = require('express');
 const cors = require('cors');
+// Imports
+const router = require('./router');
 
 // Init du serveur
 const app = express();
@@ -8,5 +10,6 @@ const app = express();
 // On branche nos middlewares
 app.use(cors());
 app.use(express.json());
+app.use(router);
 
 module.exports = app;
--- index.js
+++ index.js
@@ -80,58 +80,6 @@ const app = require('./app/server');
 // On déifnit le port
 const PORT = 3000;
 
-const tasks = require('./data/tasks.json');
-//* === Tasks ===
-app.get('/tasks', function(request, response) {
-  console.log(`${request.method} ${request.originalUrl}`);
-
-  response.send(tasks);
-
-  // Si on ne renvois rien au client, le processus de réponse tourne indéfiniment.
-  // Pour terminer ce process, on utilise la méthode end() de l'objet Response
-  // response.end();
-});
-// On peut mettre en place des endpoints dynamiques avec les ":" pour le param d'URL
-app.get('/tasks/:id', function(req, res) {
-  console.log(`${req.method} ${req.originalUrl}`);
-
-  //https://expressjs.com/fr/4x/api.html#req.params
-  // le param d'URL est de type string, il faut faire la conversion
-  const taskId = parseInt(req.params.id, 10);
-  const task = tasks.find((task) => task.id === taskId);
-  // On renvois au client la tâche au format JSON
-  res.json(task);
-});
-app.post('/tasks', function(req, res) {
-  console.log(`${req.method} ${req.originalUrl}`);
-
-  if(req.body.label === undefined || req.body.label === "") {
-    return res.status(400).end('The task\'s name is empty');
-  };
-
-  // On prépare la nouvelle tâche
-  const newTask = {
-    id: 99, // On vera plus tard comment faire ça prorpement
-    label: req.body.label,
-    done: false,
-    userId: 1
-  };
-
-  // On l'a rajoute dans le tableau des tâches
-  try {
-    tasks.push(newTask);
-    // Si la création de la ressource est ok
-    // on répond un code 201 Created et on envoi la nouvelle tâche
-    res.status(201).json(newTask);
-  } catch (error) {
-    // Si la création ne passe pas
-    // on répond un code 500 Internal Error et on termine le processus de réponse
-    console.error(error);
-    res.status(500).end(error);
-  }
-});
-
-
 // On met en écoute le serveur sur le port définit plus haut
 app.listen(PORT, function() {
   console.log(`App is listening on port ${PORT}`);
